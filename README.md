7a8e64d8c10ade9a13de1e7b2dd1a58d5a940b52
geekchanskiy <astrovskiy.04@gmail.com>
2025-08-04 00:13:15 +0300

    generator works :)

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ https://pkg.go.dev/cmd/go/internal/generate
 https://pkg.go.dev/go/ast#pkg-overview
 https://pkg.go.dev/go/parser#ParseFile
 
+https://medium.com/swlh/cool-stuff-with-gos-ast-package-pt-1-981460cddcd7
+
 // Code generated via enum_codegen DO NOT EDIT
--- test_package/test_file.go
+++ test_package/test_file.go
@@ -1,9 +1,19 @@
 package test_package
 
-//go:generate main
-type Enum string
+//go:generate main "enumValue1" "enumValue2"
+type Enum int
 
-//go:generate main
+const (
+	EnumValue Enum = iota
+	EnumValue2
+	EnumValue3
+	EnumValue4
+)
+
+const (
+	EnumValue5, EnumValue6 Enum = 5, 6
+)
+
 type Test struct {
 	Value string
 }
