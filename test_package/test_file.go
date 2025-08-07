663d5e49bb1b0bfbd3b394b47d18b506a68e04cd
astrovsky-d <dmitry.astrovsky@betera.by>
2025-08-07 13:51:01 +0300

    enum_codegen ready

--- test_package/test_file.go
+++ test_package/test_file.go
@@ -4,16 +4,14 @@ package test_package
 type Enum int
 
 const (
-	EnumValue1 Enum = iota // Translate="Значение Enum 1"
-	EnumValue2             // Translate="Значение Enum 2"
-	EnumValue3             // Translate="Значение Enum 3"
-	EnumValue4             // Translate="Значение Enum 4"
+	// Undefined Translate="Значение Enum не определено"
+	Undefined Enum = iota
+	// EnumValue1 Translate="Значение Enum 1"
+	EnumValue1
+	// EnumValue2 Translate="Значение Enum 2"
+	EnumValue2
+	// EnumValue3 Translate="Значение Enum 3"
+	EnumValue3
+	// Translate="Значение Enum 4"
+	EnumValue4
 )
-
-const (
-	EnumValue5, EnumValue6 Enum = 5, 6
-)
-
-type Test struct {
-	Value string
-}
